09878ae16d2cbb58080f68d34a51e27c546d0d3e
Rebecca Wardle <mattydwardle@gmail.com>
2020-01-13 17:07:15 +0000

    Started writing ordered permutations in java to test time

---
+++ OrderedPermutations.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class OrderedPermutations
+{
+    public void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("How many numbers: ");
+        int length = keyboard.nextInt();
+        int[] list = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            list[i] = i;
+        }
+
+        int swapper = 0, initial_comp = 0, rearrange = 0, asc_swapper = 0, count = 1;
+        boolean last = false;
+
+
+
+    }
+}
+
