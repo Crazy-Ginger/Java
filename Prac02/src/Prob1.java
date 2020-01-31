8f74fb8855c071f42087f1d6eab010ff5fc83345
Matthew Wardle <mattydwardle@gmail.com>
2020-01-31 18:22:19 +0000

    Prac03 implementing vector classes, last question not done

--- Prac02/src/Prob1.java
+++ Prac02/src/Prob1.java
@@ -6,26 +6,30 @@ public class Prob1
     {
         int[] resampled = new int [datapoints.length * 2 - 1];
         
-        for (int i = 0; i < resampled.length; i++)
+        for (int i = 0; i < resampled.length; i+= 2)
         {
-            if (i%2 == 0)
-            {
-                System.out.println(datapoints[i/2]);
                 resampled[i] = datapoints[i/2];
-            }
-            else
-            {
-                System.out.println(datapoints[i/2 - 1] + " "+ datapoints[i/2  + 1]);
-                resampled[i] = (datapoints[i/2-1] + datapoints[i/2 +1])/2;
-            }
+        } 
+        for (int i = 1; i < resampled.length; i += 2)
+        {
+            resampled[i] = (resampled[i-1]+resampled[i+1])/2;
         }
         return resampled;
+    }
+
+    public static int[] resample(int[] datapoints, int scale)
+    {
+        
     }
 
     public static void main(String[] args)
     {
         int[] list = {1, 5, 13, 21};
-        System.out.println(resample(list));
+        int[] retList = resample(list);
 
+        for (int o: retList)
+        {
+            System.out.print(o + ", ");
+        }
     }
 }
---
+++ Prac03/src/Vector.java
@@ -0,0 +1,114 @@
+public class Vector
+{
+    private double[] params;
+
+    public Vector (double[] args)
+    {
+        this.params = args.clone();
+    }
+
+    public String toString()
+    {
+        StringBuilder out = new StringBuilder();
+        if (this.params.length < 1)
+        {
+            return "";
+        }
+        out.append("[ ");
+        for (int i = 0; i < this.params.length; i++)
+        {
+            out.append(this.params[i]);
+            if (i != this.params.length -1)
+            {
+                out.append(", ");
+            }
+        }
+        out.append("]");
+        return out.toString();
+    }
+
+    public int size()
+    {
+        return this.params.length;
+    }
+
+    public double get(int index)
+    {
+        if (index < this.params.length)
+        {
+            return this.params[index];
+        }
+        return 0;
+    }
+
+    public void set(int index, double value)
+    {
+        if (index < this.params.length)
+        {
+            this.params[index] = value;
+        }
+    }
+
+    public Vector scalarProduct(double scalar)
+    {   
+        double[] newParams = this.params.clone();
+        for (int i = 0; i < newParams.length; i++)
+        {
+            newParams[i] = newParams[i] * scalar;
+        }
+        return new Vector(newParams);
+    }
+
+    public Vector add(Vector other)
+    {
+        if (this.params.length != other.params.length)
+        {
+            return null;
+        }
+        double[] newParams = new double[this.params.length];
+        for (int i = 0; i < this.params.length; i++)
+        {
+            newParams[i] = this.params[i] + other.get(i);
+        }
+        return new Vector(newParams);
+    }
+
+    public boolean equals(Object other)
+    {
+        
+        if (other instanceof Vector)
+        {
+            try
+            {
+                Class cls = other.getClass();
+                Method getLabel = cls.getMethod("size", null);
+                int length = (int)getLabel.invoke(other, null);
+                
+                if (length == this.size())
+                {
+
+                }
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        else 
+        {
+            return false;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Vector a = new Vector(new double[] {2.0, 3.0, 4.0});
+        System.out.println(a);
+
+        // Vector b = new Vector(new double[] {4.0, 3.0, 2.0});
+        // System.out.println(b.toString());
+        // Vector c = a.add(b);
+        // System.out.println(c.toString());
+    }
+}
