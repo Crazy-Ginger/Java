2b02fa95f72fbe1ed2547dd6e96b9374d536abf4
Rebecca Wardle <mattydwardle@gmail.com>
2020-02-08 00:12:23 +0000

    Looked at implementing triangles

--- Prac04/src/Shape.java
+++ Prac04/src/Shape.java
@@ -3,17 +3,36 @@ public class Shape
     private String colour;
     private boolean filled;
     private int coVals;
+    private double centreX, centreY;
 
     //class constructors
     public Shape()
     {
         this.colour = "red";
         this.filled = true;
+        this.centreX = 0;
+        this.centreY = 0;
     }
     public Shape(String col, boolean fill)
     {
         this.colour = col;
         this.filled = fill;
+        this.centreX = 0;
+        this.centreY = 0;
+    }
+    public Shape(String col, boolean fill, double x, double y)
+    {
+        this.colour = col;
+        this.filled = fill;
+        this.centreX = x;
+        this.centreY = y;
+    }
+    public Shape(double x, double y)
+    {
+        this.colour = "red";
+        this.filled = true;
+        this.centreX = x;
+        this.centreY = y;
     }
 
     //getters
@@ -40,6 +59,14 @@ public class Shape
     {
         return this.coVals;
     }
+    public double getX()
+    {
+        return this.centreX;
+    }
+    public double getY()
+    {
+        return this.centreY;
+    }
 
     //setters
     public void setColour(String col)
@@ -56,7 +83,20 @@ public class Shape
         this.colour = "";
         this.coVals = value;
     }
-    
+    public void setX(double x)
+    {
+        this.centreX = x;
+    }
+    public void setY(double y)
+    {
+        this.centreY = y;
+    }    
+    public void setCentre(double x, double y)
+    {
+        this.centreX = x;
+        this.centreY = y;
+    }
+
     @Override
     public String toString()
     {
