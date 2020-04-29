0ede00814e1c5a0b4d8e2306b440464cda53e160
Rebecca Wardle <mattydwardle@gmail.com>
2020-04-29 04:48:49 +0100

    Written tests but can't run

--- Summer/src/tsp/HamiltonainTester.java
+++ Summer/src/tsp/HamiltonainTester.java
@@ -1,5 +1,7 @@
 package tsp;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
@@ -8,6 +10,50 @@ public class HamiltonainTester
     @Test
     public void testIsHamiltonian()
     {
-        //assertEquals
+        Hamiltonian h = new Hamiltonian();
+
+        int[][] test1 = {{0, 1, 0, 1, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 1},
+                        {0, 1, 1, 1, 0}};
+        int[][] test2 =  {{0, 1, 0, 1, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 0},
+                        {0, 1, 1, 0, 0}};
+        int[][] test3 =  {{0, 1, 0, 0, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 1},
+                        {0, 1, 1, 1, 0}};
+        assertTrue(h.testIsHamiltonian(test1));
+        assertFalse(h.testIsHamiltonian(test2));
+        assertFalse(h.testIsHamiltonian(test3));
+    }
+
+    @Test
+    public void testHamBackTrack()
+    {
+        Hamiltonian h = new Hamiltonian();
+
+        int[][] test1 = {{0, 1, 0, 1, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 1},
+                        {0, 1, 1, 1, 0}};
+        int[][] test2 =  {{0, 1, 0, 1, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 0},
+                        {0, 1, 1, 0, 0}};
+        int[][] test3 =  {{0, 1, 0, 0, 0},
+                        {1, 0, 1, 1, 1},
+                        {0, 1, 0, 0, 1},
+                        {1, 1, 0, 0, 1},
+                        {0, 1, 1, 1, 0}};
+        assertTrue(h.testHamBackTrack(test1));
+        assertFalse(h.testHamBackTrack(test2));
+        assertFalse(h.testHamBackTrack(test3));
     }
 }
